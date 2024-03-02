99547b41a6a0e0b7214f57627c17f557878593cb
AhdaR-IT <ahdarizki30@gmail.com>
2024-03-02 15:33:43 +0700

    Menjadikan 1 folder

--- visimisi/script.js
+++
@@ -1,6 +0,0 @@
-
-  const visiMisiLink = document.querySelector('.nav-link.active');
-
-  visiMisiLink.addEventListener('click', (event) => {
-    event.preventDefault(); // Mencegah perpindahan halaman
-  });
