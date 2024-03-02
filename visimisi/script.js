f8494212a88ca0e931ce4cf3e0b6041cb3a835b4
AhdaR-IT <ahdarizki30@gmail.com>
2024-03-02 14:41:35 +0700

    coba

--- visimisi/script.js
+++ visimisi/script.js
@@ -1,7 +1,6 @@
 
-  const visiMisiLink = document.querySelector('.nav-link.active');
+  // const visiMisiLink = document.querySelector('.nav-link.active');
 
-  visiMisiLink.addEventListener('click', (event) => {
-    event.preventDefault(); // Mencegah perpindahan halaman
-    // Tambahkan logika lain yang Anda inginkan di sini
-  });
+  // visiMisiLink.addEventListener('click', (event) => {
+  //   event.preventDefault(); // Mencegah perpindahan halaman
+  // });
