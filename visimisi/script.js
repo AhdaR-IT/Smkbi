cdf98bfe54779d7820cfe0d7aeec91fe6b8becdb
AhdaR-IT <ahdarizki30@gmail.com>
2024-03-02 12:23:18 +0700

    menambahkan code js

---
+++ visimisi/script.js
@@ -0,0 +1,7 @@
+
+  const visiMisiLink = document.querySelector('.nav-link.active');
+
+  visiMisiLink.addEventListener('click', (event) => {
+    event.preventDefault(); // Mencegah perpindahan halaman
+    // Tambahkan logika lain yang Anda inginkan di sini
+  });
